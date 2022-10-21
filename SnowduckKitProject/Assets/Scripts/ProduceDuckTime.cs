ffa7251adc063ca312f72db25c874e8ec1f7f4e0
Dongmin88 <darkbear88@gmail.com>
2022-10-22 00:31:10 +0900

    Duck Produce

---
+++ SnowduckKitProject/Assets/Scripts/ProduceDuckTime.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//임시 시간 타임
+//UI 시간에 맞춰서 조금 후에 다시 설정
+public class ProduceDuckTime : MonoBehaviour
+{
+    public float ProduceTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProduceTime += Time.deltaTime;
+    }
+}
