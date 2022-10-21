9b4837de5724f3ab6ab1ad4a57ac25db00d29a2b
jmjeon55335 <jmjeon5533@gmail.com>
2022-10-22 02:06:02 +0900

    ToyPanel1
    리스트로 토이 채우기 해야함

---
+++ SnowduckKitProject/Assets/Toy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toy : ScriptableObject
+{
+    public string ToyName;
+    public Sprite ToyImage;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ SnowduckKitProject/Assets/UIManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    RectTransform ToyPanel; //토이 패널
+    int RectPosition;
+    [SerializeField]
+    bool isToyPanel; //토이 패널이 열림/닫힘
+
+    [SerializeField]
+    Image[] ToyImage = new Image[7];
+    public List<Toy> Toys;
+    void Start()
+    {
+        ToyPanel.anchoredPosition = new Vector2(-1100, 30);
+        isToyPanel = false;
+        //토이 패널 : 닫힘
+        for (int i = 0; i < 7; i++)
+        {
+            ToyImage[i].enabled = false;
+        }
+    }
+
+    
+    void Update()
+    {
+        ToyPanelM();
+    }
+    public void ToypanelB() //토이패널 버튼
+    {
+        isToyPanel = !isToyPanel;
+    }
+    void ToyPanelM() //토이패널 움직임
+    {
+        if (isToyPanel)
+            RectPosition = 0;
+        else
+            RectPosition = -1100;   
+        
+        ToyPanel.anchoredPosition 
+            = new Vector2(Mathf.Lerp(ToyPanel.anchoredPosition.x, RectPosition, 0.05f), 30);
+    }
+
+}
