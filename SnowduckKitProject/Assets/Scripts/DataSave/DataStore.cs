407eeb95e588da9cc1a36b6113a179766f9801e1
KuroIkaNeko <cdbw02@naver.com>
2022-10-22 08:32:01 +0900

    saveupdate
    a

--- SnowduckKitProject/Assets/Scripts/DataSave/DataStore.cs
+++ SnowduckKitProject/Assets/Scripts/DataSave/DataStore.cs
@@ -7,14 +7,13 @@ public class SaveData
     public List<GameObject> SaveObjects = new List<GameObject>();
     public int Point;
     public List<string> Achievement = new List<string>();
-
 }
 
 public class DataStore : MonoBehaviour
 {
     private static SaveData _Save = new SaveData();
+    public GameObject[] AllPrefebs;
 
-    
     public static bool SetAchievement(string _Achievement)// 도감 저장. 아이템 획득할 때마다 실행해주세요.
     {
         if (!_Save.Achievement.Contains(_Achievement))
@@ -25,18 +24,16 @@ public class DataStore : MonoBehaviour
         return false;
     }
 
-    public static List<string> SetAchievement()
+    public static List<string> GetAchievement()
     {
         return _Save.Achievement;
     }
 
-    
     public static void AddObject (GameObject _NewObject)// 오브젝트 생성 시 동작시켜 주세요.
     {
         _Save.SaveObjects.Add(_NewObject);
     }
 
-    
     public static void RemoveObject(GameObject _NewObject)// 오브젝트 제거 시 동작시켜 주세요.
     {
         _Save.SaveObjects.Remove(_NewObject);
@@ -62,14 +59,32 @@ public class DataStore : MonoBehaviour
     private void Start()
     {
         Load();
+        InvokeRepeating("Save", 60f, 60f);
     }
 
     private void Load()
     {
         string _SaveData = PlayerPrefs.GetString("Save");
+        Debug.Log(_SaveData);
+        SaveDataFormat nSDF = new SaveDataFormat();
         if (_SaveData != null)
         {
-            _Save = JsonUtility.FromJson<SaveData>(_SaveData);
+            nSDF = JsonUtility.FromJson<SaveDataFormat>(_SaveData);
+            _Save.Point = nSDF.Point;
+            _Save.Achievement = nSDF.Achievement;
+
+            for (int i = 0; i < nSDF.Name.Count ; i++)
+            {
+                foreach(GameObject nOb in AllPrefebs)
+                {
+                    if(nOb.name == nSDF.Name[i])
+                    {
+                        GameObject nGO = Instantiate((GameObject)nOb, nSDF.Pos[i], Quaternion.Euler(nSDF.Rot[i]));
+                        _Save.SaveObjects.Add(nGO);
+                        break;
+                    }
+                }
+            }
         }
         else
         {
@@ -77,16 +92,41 @@ public class DataStore : MonoBehaviour
         }
     }
 
-
-    private void OnDestroy()
+    private void OnApplicationQuit()
     {
         Save();
     }
 
+    //private void OnDestroy()
+    //{
+    //    Save();
+    //}
+
     private void Save()
     {
-        string _SaveData = JsonUtility.ToJson(_Save);
+        SaveDataFormat nSDF = new SaveDataFormat();
+        nSDF.Point = GetPoint();
+        foreach(GameObject nGO in _Save.SaveObjects)
+        {
+            nSDF.Name.Add(nGO.name.Substring(0, nGO.name.Length - 7));
+            nSDF.Pos.Add(nGO.transform.position);
+            nSDF.Rot.Add(nGO.transform.rotation.eulerAngles);
+        }
+        nSDF.Achievement = _Save.Achievement;
+
+        string _SaveData = JsonUtility.ToJson(nSDF);
         PlayerPrefs.SetString("Save", _SaveData);
+        Debug.Log(_SaveData);
     }
 
+}
+
+[SerializeField]
+public class SaveDataFormat
+{
+    public int Point;
+    public List<string> Name = new List<string>();
+    public List<Vector3> Pos = new List<Vector3>();
+    public List<Vector3> Rot = new List<Vector3>();
+    public List<string> Achievement = new List<string>();
 }
