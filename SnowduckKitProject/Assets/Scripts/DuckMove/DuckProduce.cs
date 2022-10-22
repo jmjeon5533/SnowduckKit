d1c3c23deb9161fa69b82d9363cab0dfeb0c2033
Dongmin88 <darkbear88@gmail.com>
2022-10-22 14:00:09 +0900

    Duck Obj

--- SnowduckKitProject/Assets/Scripts/DuckMove/DuckProduce.cs
+++ SnowduckKitProject/Assets/Scripts/DuckMove/DuckProduce.cs
@@ -7,10 +7,12 @@ public class DuckProduce : MonoBehaviour
     public float ProduceTimer;
     public float MaxTimer;
     public GameObject DuckObject;
+    public GameObject DuckObject2;
     public Vector3 MinPosition;
     public Vector3 MaxPosition;
     public GameObject FloorGameobject;
     public float ProduceHeight;
+    public float GaugeClickAmount;
 
     // Start is called before the first frame update
     void Start()
@@ -25,7 +27,19 @@ public class DuckProduce : MonoBehaviour
         if (ProduceTimer > MaxTimer)
         {
             ProduceTimer = 0f;
-            Instantiate(DuckObject, new Vector3(Random.Range(MinPosition.x, MaxPosition.x), FloorGameobject.transform.position.y + ProduceHeight, Random.Range(MinPosition.z, MaxPosition.z)),Quaternion.identity);
+            int RandomInt = Random.Range(0, 2);
+            if (RandomInt == 0)//¿À¸®1
+            {
+                Instantiate(DuckObject, new Vector3(Random.Range(MinPosition.x, MaxPosition.x), FloorGameobject.transform.position.y + ProduceHeight, Random.Range(MinPosition.z, MaxPosition.z)), Quaternion.identity);
+            }
+            else if (RandomInt == 1)
+            {
+                Instantiate(DuckObject2, new Vector3(Random.Range(MinPosition.x, MaxPosition.x), FloorGameobject.transform.position.y + ProduceHeight, Random.Range(MinPosition.z, MaxPosition.z)), Quaternion.identity);
+            }
         }
+    }
+    public void GaugeButtonClick()
+    {
+        ProduceTimer += GaugeClickAmount;
     }
 }
