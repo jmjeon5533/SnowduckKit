d4d76ff73588b0ebb38ae0ec2ae1d3a12b38d0d5
jmjeon55335 <jmjeon5533@gmail.com>
2022-10-23 09:53:42 +0900

    Revert "Merge remote-tracking branch 'origin/main'"
    This reverts commit 62d79015d540ef5f89d4459c335a963a3d7b70fc, reversing
    changes made to 944c20a367fb3d8c88fbfee89c579980693b4706.

--- SnowduckKitProject/Assets/Scripts/UI/ToyDatabase.cs
+++ SnowduckKitProject/Assets/Scripts/UI/ToyDatabase.cs
@@ -32,10 +32,4 @@ public class ToyDatabase : MonoBehaviour
         Buttons.Remove(_Target);
         Destroy(_Target);
     }
-
-    private void Update()
-    {
-        RectTransform rectTransform = GetComponent<RectTransform>();
-        rectTransform.sizeDelta = new Vector2(Buttons.Count * 95 , 0);
-    }
 }
